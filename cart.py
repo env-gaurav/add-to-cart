4756f03120756fd5f81fd9363a32efdb4854654f
env-gaurav <gaurav2528singh@gmail.com>
2025-01-27 10:20:39 +0530

    cart.py

--- cart.py
+++ cart.py
@@ -1,17 +1,28 @@
+basket=[]
 shopping_mall='''
-
 1. Apple    2. Banana   
 3. Litchi   4. Grapes
+5. Exit
 '''
 print(shopping_mall)
-choice= input("Enter your choice: ")
-if choice == "1":
-    print("Apple")
-elif choice == "2":
-    print("Banana")
-elif choice == "3":
-    print("Litchi")
-elif choice == "4":
-    print("Grapes")
-else:
-    print("provide correct input")
+while True:
+    choice= input("Enter your choice: ")
+    if choice== "5":
+        print("Exiting the program")
+        break
+    elif choice == "1":
+        basket.append("Apple")
+        print("Apple added to cart")
+    elif choice == "2":
+        basket.append("Banana")
+        print("Banana added to cart")
+    elif choice == "3":
+        basket.append("Litchi")
+        print("Litchi added to cart")
+    elif choice == "4":
+        basket.append("Grapes")
+        print("Grapes added to cart")
+    else:
+        print("provide correct input")
+
+print(basket)
